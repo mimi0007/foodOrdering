79cf83a106429d0722d4f93fe1c5adc415a0f4a3
mimi0007 <afsanaaziz.mimi@gmail.com>
2023-01-21 01:31:07 +0600

    Initial Setup

---
+++ src/main/java/com/example/foodorderingdemo/FoodOrderingDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.foodorderingdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodOrderingDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodOrderingDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/foodorderingdemo/FoodOrderingDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.foodorderingdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodOrderingDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
